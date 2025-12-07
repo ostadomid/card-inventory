285b3462794a016ae28e4e93c938d3112c037813
OstadOmid <ostadomid@users.noreply.github.com>
2025-12-08 03:11:15 +0330

    add initial tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,18 @@
-import { config } from 'dotenv'
+/* eslint-disable @typescript-eslint/no-namespace */
+import 'dotenv/config'
 
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import Database from 'better-sqlite3'
 
 import * as schema from './schema.ts'
 
-config()
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DATABASE_URL: string
+    }
+  }
+}
 
-const sqlite = new Database(process.env.DATABASE_URL!)
+const sqlite = new Database(process.env.DATABASE_URL)
 export const db = drizzle(sqlite, { schema })
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,36 @@
-import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core'
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { sql } from 'drizzle-orm'
 
-export const todos = sqliteTable('todos', {
-  id: integer('id', { mode: 'number' }).primaryKey({
-    autoIncrement: true,
-  }),
-  title: text('title').notNull(),
-  createdAt: integer('created_at', { mode: 'timestamp' }).default(
+// export const todos = sqliteTable('todos', {
+//   id: integer('id', { mode: 'number' }).primaryKey({
+//     autoIncrement: true,
+//   }),
+//   title: text('title').notNull(),
+//   createdAt: integer('created_at', { mode: 'timestamp' }).default(
+//     sql`(unixepoch())`,
+//   ),
+// })
+
+export const users = sqliteTable('users', {
+  phone: text().primaryKey(),
+  firstName: text('first_name'),
+  lastName: text('last_name'),
+  password: text(),
+  active: integer({ mode: 'boolean' }).default(true),
+})
+export const cards = sqliteTable('cards', {
+  id: text().primaryKey(),
+  price: integer({ mode: 'number' }).default(0),
+})
+
+export const orders = sqliteTable('orders', {
+  id: integer().primaryKey({ autoIncrement: true }),
+  price: integer(),
+  amount: integer(),
+  createdAd: integer('created_at', { mode: 'timestamp' }).default(
     sql`(unixepoch())`,
   ),
+  by: integer('ordered_by')
+    .notNull()
+    .references(() => users.phone),
 })
