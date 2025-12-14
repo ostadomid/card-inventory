962b8d8cf9ec5ad315ea7db2caf1a593da83da37
ostadomid <ostadomid2@gmail.com>
2025-12-14 20:36:47 +0330

    change s3 provider

--- src/routes/api/upload.ts
+++ src/routes/api/upload.ts
@@ -1,26 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router"
 
 import { handleRequest, route } from "@better-upload/server"
-import { custom } from "@better-upload/server/clients"
+import { tigris } from "@better-upload/server/clients"
 import type { Router } from "@better-upload/server"
 
 const router: Router = {
   bucketName: "card-inventory",
-  client: custom({
-    host: "localhost:8333",
-    region: "alaki",
-    accessKeyId: "super_secure_key",
-    secretAccessKey: "super_secure_secret",
-    secure: false,
-    forcePathStyle: true,
+  client: tigris({
+    endpoint:"https://t3.storage.dev",
+    accessKeyId:"tid_TmyloVuvHBFBcgiDXLCLUEwtHpGEtyUNsirtKPeTtGpvsHMNUh",
+     secretAccessKey:"tsec_c8_wMArRcOvuXetyRe+6GjM-gjFLU4yYLGZCXo6W-Zc28NYGTNmv19SFJj7kS5LthyDs6A"
+    // host: "localhost:8333",
+    // region: "alaki",
+    // accessKeyId: "super_secure_key",
+    // secretAccessKey: "super_secure_secret",
+    // secure: false,
+    // forcePathStyle: true,
   }),
 
   routes: {
     cards: route({
       fileTypes: ["image/*"],
       maxFileSize: 35 * 1024 * 1024,
-      onBeforeUpload(data) {
-        console.log({ data })
+      onAfterSignedUrl(data) {
+        // console.log({data})
+        return data.file.objectInfo
       },
     }),
   },
